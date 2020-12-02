a65b7ea389826bf726de13bd22f44855aa8fd9b6
Michael Jones <mjones@kotrs.com>
2020-12-02 10:52:47 -0500

    Attempting to fix issue where icon does not show back up in dock when opening the main window again

--- public/electron.js
+++ public/electron.js
@@ -1,29 +1,29 @@
-const { app, BrowserWindow, Tray, ipcMain } = require("electron");
-const path = require("path");
-const isDev = require("electron-is-dev");
-const storage = require("electron-json-storage");
-const { autoUpdater } = require("electron-updater");
-const log = require("electron-log");
-const { FetchAddons, FetchInstalledAddons } = require("./TukuiService");
+const { app, BrowserWindow, Tray, ipcMain } = require('electron')
+const path = require('path')
+const isDev = require('electron-is-dev')
+const storage = require('electron-json-storage')
+const { autoUpdater } = require('electron-updater')
+const log = require('electron-log')
+const { FetchAddons, FetchInstalledAddons } = require('./TukuiService')
 
-let mainWindow, trayWindow, tray;
+let mainWindow, trayWindow, tray, lastAddonPull
 
 const startup = () => {
-  autoUpdater.autoDownload = false;
-  if (!app.getLoginItemSettings().wasOpenedAtLogin) createMainWindow();
-  else app.dock.hide();
-  updateAddonInfo();
-  createTrayWindow();
-  setOpenAtLogin();
-  if (!isDev) autoUpdater.checkForUpdates();
-};
+  autoUpdater.autoDownload = false
+  if (!app.getLoginItemSettings().wasOpenedAtLogin) createMainWindow()
+  else app.dock.hide()
+  updateAddonInfo()
+  createTrayWindow()
+  setOpenAtLogin()
+  if (!isDev) autoUpdater.checkForUpdates()
+}
 
-const updateAddonInterval = 1000 * 60;
+const updateAddonInterval = 1000 * 60
 const updateAddonInfo = () => {
   FetchAddons((addons) => {
     try {
-      const installedRetailAddons = FetchInstalledAddons("Retail");
-      const installedClassicAddons = FetchInstalledAddons("Classic");
+      const installedRetailAddons = FetchInstalledAddons('Retail')
+      const installedClassicAddons = FetchInstalledAddons('Classic')
 
       const result = {
         Retail: {
@@ -31,100 +31,105 @@ const updateAddonInfo = () => {
           elvui: {
             ...addons.Retail.elvui,
             localAddon: installedRetailAddons?.filter(
-              (addon) => addon.name === "ElvUI"
+              (addon) => addon.name === 'ElvUI'
             )[0],
           },
           tukui: {
             ...addons.Retail.tukui,
             localAddon: installedRetailAddons?.filter(
-              (addon) => addon.name === "Tukui"
+              (addon) => addon.name === 'Tukui'
             )[0],
           },
-          all:  installedRetailAddons ? addons.Retail.all.map((addon) => {
-            return {
-              ...addon,
-              localAddon: installedRetailAddons.filter(
-                (a) => a.name === addon.name
-              )[0],
-            };
-          }) : addons.Retail.all,
+          all: installedRetailAddons
+            ? addons.Retail.all.map((addon) => {
+                return {
+                  ...addon,
+                  localAddon: installedRetailAddons.filter(
+                    (a) => a.name === addon.name
+                  )[0],
+                }
+              })
+            : addons.Retail.all,
         },
         Classic: {
           installed: installedClassicAddons ? true : false,
           elvui: {
             ...addons.Classic.elvui,
             localAddon: installedClassicAddons?.filter(
-              (addon) => addon.name === "ElvUI"
+              (addon) => addon.name === 'ElvUI'
             )[0],
           },
           tukui: {
             ...addons.Classic.tukui,
             localAddon: installedClassicAddons?.filter(
-              (addon) => addon.name === "Tukui"
+              (addon) => addon.name === 'Tukui'
             )[0],
           },
-          all: installedClassicAddons ? addons.Classic.all.map((addon) => {
-            return {
-              ...addon,
-              localAddon: installedClassicAddons.filter(
-                (a) => a.name === addon.name
-              )[0],
-            };
-          }) : addons.Classic.all,
+          all: installedClassicAddons
+            ? addons.Classic.all.map((addon) => {
+                return {
+                  ...addon,
+                  localAddon: installedClassicAddons.filter(
+                    (a) => a.name === addon.name
+                  )[0],
+                }
+              })
+            : addons.Classic.all,
         },
-      };
+      }
       if (mainWindow?.isVisible())
-        mainWindow.webContents.send("update-addons", result);
+        mainWindow.webContents.send('update-addons', result)
+      lastAddonPull = result
     } catch {}
-    setTimeout(updateAddonInfo, updateAddonInterval);
-  });
-};
+    setTimeout(updateAddonInfo, updateAddonInterval)
+  })
+}
 
 //#region Functions
 const setOpenAtLogin = () => {
-  storage.get("auto-update", (err, data) => {
-    const autoStart = err || data;
+  storage.get('auto-update', (err, data) => {
+    const autoStart = err || data
     app.setLoginItemSettings({
       openAtLogin: autoStart,
-    });
-  });
-};
+    })
+  })
+}
 //#endregion
 
 //#region Inter-app communication
-ipcMain.on("main", (event, eventName, arg1) => {
-  if (eventName === "icon_click") toggleMainWindow();
-  else if (eventName === "update_changed") {
-    const autoUpdate = arg1;
-    storage.set("auto-update", autoUpdate, () => {
+ipcMain.on('main', (event, eventName, arg1) => {
+  if (eventName === 'icon_click') toggleMainWindow()
+  else if (eventName === 'update_changed') {
+    const autoUpdate = arg1
+    storage.set('auto-update', autoUpdate, () => {
       app.setLoginItemSettings({
         openAtLogin: autoUpdate,
-      });
-      if (!autoUpdate && (!mainWindow || !mainWindow.isVisible())) app.quit();
-    });
-  } else if (eventName === "update_requested") {
-    autoUpdater.downloadUpdate();
-  } else if (eventName === "update_install") {
-    autoUpdater.quitAndInstall();
+      })
+      if (!autoUpdate && (!mainWindow || !mainWindow.isVisible())) app.quit()
+    })
+  } else if (eventName === 'update_requested') {
+    autoUpdater.downloadUpdate()
+  } else if (eventName === 'update_install') {
+    autoUpdater.quitAndInstall()
   }
-});
+})
 //#endregion
 
 //#region Main Window
-const allWindowsClosed = () => app.quit();
+const allWindowsClosed = () => app.quit()
 const mainWindowClosed = () => {
-  mainWindow = null;
+  mainWindow = null
 
-  storage.get("auto-update", (err, obj) => {
-    if (err || obj) app.dock.hide();
-    else app.quit();
-  });
-};
+  storage.get('auto-update', (err, obj) => {
+    if (err || obj) app.dock.hide()
+    else app.quit()
+  })
+}
 
 const createMainWindow = () => {
   if (mainWindow != null) {
-    mainWindow.show();
-    return;
+    mainWindow.show()
+    return
   }
   mainWindow = new BrowserWindow({
     width: 1550,
@@ -135,40 +140,42 @@ const createMainWindow = () => {
       devTools: isDev,
       webSecurity: false,
     },
-    backgroundColor: "#000",
-    titleBarStyle: "hiddenInset",
-  });
+    backgroundColor: '#000',
+    titleBarStyle: 'hiddenInset',
+  })
   mainWindow.loadURL(
     isDev
-      ? "http://localhost:3000"
-      : `file://${path.join(__dirname, "../build/index.html")}`
-  );
-  mainWindow.on("closed", mainWindowClosed);
-  mainWindow.on("show", () => {
+      ? 'http://localhost:3000'
+      : `file://${path.join(__dirname, '../build/index.html')}`
+  )
+  mainWindow.on('closed', mainWindowClosed)
+  mainWindow.on('show', () => {
     //app.dock().show();
-  });
-};
+    mainWindow.webContents.send('update-addons', lastAddonPull)
+  })
+}
 
 const toggleMainWindow = () => {
   if (!mainWindow) {
-    createMainWindow();
-    app.dock.show();
+    createMainWindow()
+    app.dock.show()
   } else if (mainWindow.isVisible()) {
-    mainWindow.hide();
-    app.dock.hide();
+    mainWindow.destroy()
+    mainWindow = null
+    app.dock.hide()
   } else {
-    mainWindow.show();
-    app.dock.show();
+    mainWindow.show()
+    app.dock.show()
   }
-};
+}
 //#endregion
 
 //#region Tray window
 const createTrayWindow = () => {
-  tray = new Tray(path.join(__dirname, "trayTemplate.png"));
-  tray.on("right-click", toggleTrayWindow);
-  tray.on("double-click", toggleTrayWindow);
-  tray.on("click", toggleTrayWindow);
+  tray = new Tray(path.join(__dirname, 'trayTemplate.png'))
+  tray.on('right-click', toggleTrayWindow)
+  tray.on('double-click', toggleTrayWindow)
+  tray.on('click', toggleTrayWindow)
 
   trayWindow = new BrowserWindow({
     width: 300,
@@ -183,71 +190,71 @@ const createTrayWindow = () => {
       nodeIntegration: true,
       enableRemoteModule: true,
     },
-  });
+  })
   trayWindow.loadURL(
     isDev
-      ? "http://localhost:3000/tray.html"
-      : `file://${path.join(__dirname, "../build/tray.html")}`
-  );
-  trayWindow.on("blur", () => trayWindow.hide());
-};
+      ? 'http://localhost:3000/tray.html'
+      : `file://${path.join(__dirname, '../build/tray.html')}`
+  )
+  trayWindow.on('blur', () => trayWindow.hide())
+}
 
 const toggleTrayWindow = () => {
   if (trayWindow.isVisible()) {
-    trayWindow.hide();
+    trayWindow.hide()
   } else {
-    showTrayWindow();
+    showTrayWindow()
   }
-};
+}
 
 const showTrayWindow = () => {
-  const position = getTrayWindowPosition();
-  trayWindow.setPosition(position.x, position.y, false);
-  trayWindow.show();
-  trayWindow.focus();
-};
+  const position = getTrayWindowPosition()
+  trayWindow.setPosition(position.x, position.y, false)
+  trayWindow.show()
+  trayWindow.focus()
+}
 
 const getTrayWindowPosition = () => {
-  const windowBounds = trayWindow.getBounds();
-  const trayBounds = tray.getBounds();
+  const windowBounds = trayWindow.getBounds()
+  const trayBounds = tray.getBounds()
   const x = Math.round(
     trayBounds.x + trayBounds.width / 2 - windowBounds.width / 2
-  );
-  const y = Math.round(trayBounds.y + trayBounds.height + 4);
+  )
+  const y = Math.round(trayBounds.y + trayBounds.height + 4)
 
-  return { x: x, y: y };
-};
+  return { x: x, y: y }
+}
 //#endregion
 
 //#region App events
-app.on("ready", startup);
-app.on("window-all-closed", allWindowsClosed);
-app.on("activate", () => {
+app.on('ready', startup)
+app.on('window-all-closed', allWindowsClosed)
+app.on('activate', () => {
   if (mainWindow === null) {
-    createMainWindow();
+    createMainWindow()
   } else {
-    mainWindow.show();
+    mainWindow.show()
   }
-});
-app.on("ready-to-show", () => {
-  log.debug("ready-to-show");
-  autoUpdater.logger = log;
-});
+})
+app.on('ready-to-show', () => {
+  log.debug('ready-to-show')
+  autoUpdater.logger = log
+})
 //#endregion
 
 //#region AutoUpdater events
-autoUpdater.on("update-available", () => {
+autoUpdater.on('update-available', () => {
   //autoUpdater.downloadUpdate()
-  if (mainWindow?.isVisible()) mainWindow.webContents.send("update-available");
-  else autoUpdater.downloadUpdate();
-});
+  if (mainWindow?.isVisible()) mainWindow.webContents.send('update-available')
+  else autoUpdater.downloadUpdate()
+})
 
-autoUpdater.on("update-downloaded", () => {
-  if (mainWindow?.isVisible()) mainWindow.webContents.send("update-downloaded");
-  else autoUpdater.quitAndInstall();
-});
+autoUpdater.on('update-downloaded', () => {
+  if (mainWindow?.isVisible()) mainWindow.webContents.send('update-downloaded')
+  else autoUpdater.quitAndInstall()
+})
 
-autoUpdater.on("update-not-available", () => {
-  setTimeout(() => autoUpdater.checkForUpdates(), 1000 * 60 * 10);
-});
+autoUpdater.on('update-not-available', () => {
+  setTimeout(() => autoUpdater.checkForUpdates(), 1000 * 60 * 10)
+})
 //#endregion
